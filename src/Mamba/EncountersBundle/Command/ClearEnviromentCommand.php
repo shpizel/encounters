c407202b351e3dffdda5033f6fb46c50631b9406
Igor Shpizel <igor@shpizel.ru>
2012-01-30 18:25:20 +0400

    ignore

---
+++ src/Mamba/EncountersBundle/Command/ClearEnviromentCommand.php
@@ -0,0 +1,27 @@
+<?php
+namespace Mamba\EncountersBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * ClearEnviroment
+ *
+ * @package EncountersBundle
+ */
+class ClearEnviroment extends ContainerAwareCommand {
+
+    /**
+     * Экзекутор
+     *
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
+     * @return mixed
+     */
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        print 1;
+    }
+}
