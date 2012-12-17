6c8c7df0a2cf8d2b0108138569025c5f59badede
Igor Shpizel <igor@shpizel.ru>
2012-12-17 18:45:45 +0400

    everyday gift sender fixes

--- src/Mamba/EncountersBundle/Resources/public/js/pages/Search.js
+++ src/Mamba/EncountersBundle/Resources/public/js/pages/Search.js
@@ -161,7 +161,7 @@ $Search = {
     /**
      * Отправляет голосование и показывает следующую фотку
      *
-     * @request vote.set
+     * @request decision.set
      */
     makeDecision: function($decision) {
         if ($Search.$storage['locked'] || !$Search.$storage['currentQueueElement']) {
@@ -236,6 +236,28 @@ $Search = {
                 $("div.app-meet-button > a.app-menu1").removeClass("app-menu1-active");
                 $("div.app-meet-button > a.app-menu2").removeClass("app-menu2-active");
                 $("div.app-meet-button > a.app-menu3").removeClass("app-menu3-active");
+
+                /**
+                 * Показ лаера ежедневных подарков
+                 *
+                 * @author shpizel
+                 */
+                var $variables = $Config.get('variables');
+
+                var
+                    $last_everyday_gift_layer_shown      = $variables['last_everyday_gift_layer_shown'],
+                    $last_everyday_gift_accepted         = $variables['last_everyday_gift_accepted'],
+                    $last_everyday_gift_accepted_counter = $variables['last_everyday_gift_accepted_counter'],
+                    $time                                = $Config.get('time')
+                ;
+
+                /** Показываем лаер ежедневного подарка если подарок сегодня не был подарен и если лаер показывался последний раз более 1 часа назад */
+                if ((new Date($time * 1000)).getDate() != (new Date($last_everyday_gift_accepted * 1000)).getDate()) {
+                    if ($time - $last_everyday_gift_layer_shown > 3600) {
+                        $Layers.showEverydayGiftLayer();
+                        $Config.get('variables')['last_everyday_gift_layer_shown'] = $Tools.microtime();
+                    }
+                }
 
             } else {
                 $status = $data.status;
@@ -427,27 +449,6 @@ $Search = {
      * @run page
      */
     run: function() {
-
-        /**
-         * Показ лаера ежедневных подарков
-         *
-         * @author shpizel
-         */
-        var $variables = $Config.get('variables');
-
-        var
-            $last_everyday_gift_layer_shown      = $variables['last_everyday_gift_layer_shown'],
-            $last_everyday_gift_accepted         = $variables['last_everyday_gift_accepted'],
-            $last_everyday_gift_accepted_counter = $variables['last_everyday_gift_accepted_counter'],
-            $time                                = $Tools.microtime()
-        ;
-
-        /** Показываем лаер ежедневного подарка если подарок сегодня не был подарен и если лаер показывался последний раз более 1 часа назад */
-        if ((new Date($time * 1000)).getDate() != (new Date($last_everyday_gift_accepted * 1000)).getDate()) {
-            if ($time - $last_everyday_gift_accepted > 3600) {
-                $Layers.showEverydayGiftLayer();
-            }
-        }
 
         /**
          * Работа с очередью показа
