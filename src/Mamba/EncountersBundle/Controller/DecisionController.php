20cd6158a7ca6cdb322a73896a508e419ab76342
Igor Shpizel <igor@shpizel.ru>
2014-01-25 23:00:03 +0400

    spamqueue 1

--- src/Mamba/EncountersBundle/Controller/DecisionController.php
+++ src/Mamba/EncountersBundle/Controller/DecisionController.php
@@ -162,8 +162,9 @@ class DecisionController extends ApplicationController {
                      */
                     $Redis->lPush($spamQueueKey = "spamqueue-by-{$this->webUserId}", $this->currentUserId);
                     $spamQueueLength = $Redis->lSize($spamQueueKey);
+
                     if (0 < $spamLimit = 10 - intval($this->getRedis()->hGet("mambaspam-by-{$this->webUserId}", date("dmy")))) {
-                        if ($spamQueueLength >= 3) {
+                        if ($spamQueueLength >= 1) {
                             if ($spamQueue = $Redis->lRange($spamQueueKey, 0, $spamLimit)) {
                                 $this->json['data']['is_contact'] = true;
                                 $this->json['data']['spam_queue'] = array_map(function($el){return(int)$el;}, $spamQueue);
